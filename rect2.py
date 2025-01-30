b57d033534a40cd7b765910b033c948a45e4087a
yoel-settbon <yoel-settbon@laplateforme.io>
2025-01-30 13:08:18 +0100

    :construction: feat : tests for the zombies moves

---
+++ rect2.py
@@ -0,0 +1,15 @@
+import pygame
+
+
+class Player:
+    def __init__(self, x, y):
+        self.image = pygame.image.load('assets\images\life\lives.png')
+        self.rect = self.image.get_rect(x=x, y=y)
+        self.speed = 5
+        self.velocity = [0, 0]
+
+    def move(self):
+        self.rect.move_ip(self.velocity[0] * self.speed, self.velocity[1] * self.speed)
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
--- test.py
+++ test.py
@@ -1,18 +1,32 @@
 import pygame
-
 pygame.init()
 screen = pygame.display.set_mode((1400, 750))
 running = True
-
-image = pygame.image.load('assets\images\zombies\zombie-jet-pack.png').convert()
+image = pygame.image.load('assets\images/bombs\woman.png').convert()
+x = 0
+y = 0
+clock = pygame.time.Clock()
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.blit(image, (0, 0))
-    pygame.display.update()
+    pressed = pygame.key.get_pressed()
+    if pressed[pygame.K_LEFT]:
+        x -= 1
+    if pressed[pygame.K_RIGHT]:
+        x += 1
+    if pressed[pygame.K_DOWN]:
+        y += 1
+    if pressed[pygame.K_UP]:
+        y -= 1
+
+    screen.fill((10, 100, 100))
+    screen.blit(image, (x, y))
+    pygame.display.flip()
+    clock.tick(600)
+
 
 
 pygame.quit()
